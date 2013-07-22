cdeffdf99ae78f819ed3b3074848852d8f52ac59
Gabor Papp <gabor.papp@gmail.com>
2013-07-22 12:34:52 +0200

    fixed texture loading when no directory is found

--- Prothesis2/src/Utils.cpp
+++ Prothesis2/src/Utils.cpp
@@ -14,21 +14,28 @@ vector< pair< string, gl::Texture > > loadTextures( const fs::path &relativeDir 
 
     fs::path dataPath = app::getAssetPath( relativeDir );
 
-    for ( fs::directory_iterator it( dataPath ); it != fs::directory_iterator(); ++it )
-    {
-        if ( fs::is_regular_file( *it ) )
-        {
-			try
+	if ( dataPath.empty() )
+	{
+		app::console() << "Could not find texture directory assets/" << relativeDir.string() << std::endl;
+	}
+	else
+	{
+		for ( fs::directory_iterator it( dataPath ); it != fs::directory_iterator(); ++it )
+		{
+			if ( fs::is_regular_file( *it ) )
 			{
-				gl::Texture t = loadImage( app::loadAsset( relativeDir / it->path().filename() ) );
-				textures.push_back( pair< string, gl::Texture >( it->path().filename().string(), t ) );
+				try
+				{
+					gl::Texture t = loadImage( app::loadAsset( relativeDir / it->path().filename() ) );
+					textures.push_back( pair< string, gl::Texture >( it->path().filename().string(), t ) );
+				}
+				catch ( const ImageIoException &exc  )
+				{
+					app::console() << "Unable to load image " << it->path() << ": " << exc.what() << std::endl;
+				}
 			}
-			catch ( const ImageIoException &exc  )
-			{
-				app::console() << "Unable to load image " << it->path() << ": " << exc.what() << std::endl;
-			}
-        }
-    }
+		}
+	}
 
     return textures;
 }
